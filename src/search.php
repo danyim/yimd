d2e5c45aca3b022377058ab881a623b7920e5088
Daniel Yim <danielyim@gmail.com>
2015-10-07 03:21:29 -0500

    Added styling for search result page

--- src/search.php
+++ src/search.php
@@ -9,23 +9,24 @@ get_header(); ?>
 
 <div class="container">
 	<div class="row aa_wrap aa_search">
-		<?php if ( have_posts() ) : ?>
-			<div class="aa_header">
-				<h1 class="aa_h1">
-					<?php printf( esc_html__( 'Search Results for: %s', 'neat' ), '<span>' . get_search_query() . '</span>' ); ?>
-				</h1>
-				<!-- /.aa_h1 -->
-			</div>
-			<!-- /.aa_header -->
+		<div class="col-md-10 col-md-offset-1">
+			<?php if ( have_posts() ) : ?>
+				<div class="aa_header">
+					<h4 class="aa_h1">
+						<?php printf( 'Displaying search results for: <strong>%s</strong>', '<span>' . get_search_query() . '</span>' ); ?>
+					</h4>
+					<!-- /.aa_h1 -->
+				</div>
+				<!-- /.aa_header -->
 
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'assets/views/content', get_post_format() ); ?>
-			<?php endwhile; ?>
-			<?php the_posts_navigation(); ?>
-		<?php else : ?>
-			<?php get_template_part( 'assets/views/content', 'none' ); ?>
-		<?php endif; ?>
-
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'assets/views/content', get_post_format() ); ?>
+				<?php endwhile; ?>
+				<?php the_posts_navigation(); ?>
+			<?php else : ?>
+				<?php get_template_part( 'assets/views/content', 'none' ); ?>
+			<?php endif; ?>
+		</div>
 	</div>
 	<!-- /.aa_search -->
 	<?php get_sidebar(); ?>
