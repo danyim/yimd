b34d456eb6795c08dcc3149c1def3265d3819930
Daniel Yim <danielyim@gmail.com>
2015-10-13 01:24:53 -0500

    Removed tacky animations from page tities

--- src/page-portfolio.php
+++ src/page-portfolio.php
@@ -22,7 +22,7 @@ get_header(); ?>
 	</div>
 	<div class="container-fluid pfl pfl-1">
 		<div class="container">
-			<div class="col-md-12 content">
+			<div class="col-md-12 content content-inverse">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac turpis ut tortor fermentum mattis at sit amet neque. Phasellus volutpat libero rhoncus libero suscipit, eu blandit lacus facilisis. In consequat urna non enim vestibulum commodo. Sed aliquet laoreet finibus. Curabitur metus ante, accumsan id sodales at, euismod ut ipsum. Praesent lobortis ac neque eu tincidunt. Donec rutrum ullamcorper ipsum, eu ultrices justo gravida eget. Suspendisse maximus augue at nisl dignissim rhoncus. Sed consectetur leo vitae felis auctor, sed accumsan enim ullamcorper. Duis dignissim ornare molestie. Nam massa mi, feugiat sed sollicitudin at, hendrerit ac sapien. Sed placerat orci id metus scelerisque rutrum.</p>
 
 				<p>Etiam ligula risus, fringilla hendrerit posuere in, tincidunt dictum leo. Donec maximus eros at tincidunt ullamcorper. Quisque nec sodales felis. Fusce vehicula nisi a aliquet semper. Morbi vulputate gravida ultrices. In gravida lobortis velit, a facilisis tellus porta vitae. Sed a cursus sem, ac aliquet enim. In fermentum dignissim vehicula. Quisque eleifend sapien vitae dapibus consequat. Suspendisse nec finibus nunc. Aenean leo sapien, tempor ac dignissim in, gravida sit amet tortor. Cras non mauris in tortor aliquam venenatis.</p>
@@ -33,14 +33,21 @@ get_header(); ?>
 		<div class="container">
 			<div class="col-md-12 content">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac turpis ut tortor fermentum mattis at sit amet neque. Phasellus volutpat libero rhoncus libero suscipit, eu blandit lacus facilisis. In consequat urna non enim vestibulum commodo. Sed aliquet laoreet finibus. Curabitur metus ante, accumsan id sodales at, euismod ut ipsum. Praesent lobortis ac neque eu tincidunt. Donec rutrum ullamcorper ipsum, eu ultrices justo gravida eget. Suspendisse maximus augue at nisl dignissim rhoncus. Sed consectetur leo vitae felis auctor, sed accumsan enim ullamcorper. Duis dignissim ornare molestie. Nam massa mi, feugiat sed sollicitudin at, hendrerit ac sapien. Sed placerat orci id metus scelerisque rutrum.</p>
-
-				<p>Etiam ligula risus, fringilla hendrerit posuere in, tincidunt dictum leo. Donec maximus eros at tincidunt ullamcorper. Quisque nec sodales felis. Fusce vehicula nisi a aliquet semper. Morbi vulputate gravida ultrices. In gravida lobortis velit, a facilisis tellus porta vitae. Sed a cursus sem, ac aliquet enim. In fermentum dignissim vehicula. Quisque eleifend sapien vitae dapibus consequat. Suspendisse nec finibus nunc. Aenean leo sapien, tempor ac dignissim in, gravida sit amet tortor. Cras non mauris in tortor aliquam venenatis.</p>
 			</div>
 		</div>
 	</div>
 	<div class="container-fluid pfl pfl-3">
 		<div class="container">
+			<div class="col-md-12 content">
+				<p>Etiam ligula risus, fringilla hendrerit posuere in, tincidunt dictum leo. Donec maximus eros at tincidunt ullamcorper. Quisque nec sodales felis. Fusce vehicula nisi a aliquet semper. Morbi vulputate gravida ultrices. In gravida lobortis velit, a facilisis tellus porta vitae. Sed a cursus sem, ac aliquet enim. In fermentum dignissim vehicula. Quisque eleifend sapien vitae dapibus consequat. Suspendisse nec finibus nunc. Aenean leo sapien, tempor ac dignissim in, gravida sit amet tortor. Cras non mauris in tortor aliquam venenatis.</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid pfl pfl-4">
+		<div class="container">
 			<div class="col-md-12 content content-inverse">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac turpis ut tortor fermentum mattis at sit amet neque. Phasellus volutpat libero rhoncus libero suscipit, eu blandit lacus facilisis. In consequat urna non enim vestibulum commodo. Sed aliquet laoreet finibus. Curabitur metus ante, accumsan id sodales at, euismod ut ipsum. Praesent lobortis ac neque eu tincidunt. Donec rutrum ullamcorper ipsum, eu ultrices justo gravida eget. Suspendisse maximus augue at nisl dignissim rhoncus. Sed consectetur leo vitae felis auctor, sed accumsan enim ullamcorper. Duis dignissim ornare molestie. Nam massa mi, feugiat sed sollicitudin at, hendrerit ac sapien. Sed placerat orci id metus scelerisque rutrum.</p>
+
 				<p>Etiam ligula risus, fringilla hendrerit posuere in, tincidunt dictum leo. Donec maximus eros at tincidunt ullamcorper. Quisque nec sodales felis. Fusce vehicula nisi a aliquet semper. Morbi vulputate gravida ultrices. In gravida lobortis velit, a facilisis tellus porta vitae. Sed a cursus sem, ac aliquet enim. In fermentum dignissim vehicula. Quisque eleifend sapien vitae dapibus consequat. Suspendisse nec finibus nunc. Aenean leo sapien, tempor ac dignissim in, gravida sit amet tortor. Cras non mauris in tortor aliquam venenatis.</p>
 			</div>
 		</div>
