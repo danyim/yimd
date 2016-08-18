8d15adf03026f596206a05b4d857fac23c84a9a6
Daniel Yim <danielyim@gmail.com>
2016-08-18 01:48:06 -0700

    final touches to the portfolio page

--- src/page-portfolio.php
+++ src/page-portfolio.php
@@ -11,96 +11,98 @@
  */
 
 get_header(); ?>
-<section class="portfolio">
-    <div class="portfolio-item">
-        <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/indecks-1024x672.png');">
-        </figure>
-        <figcaption>
-            <h4>indecks <a href="https://github.com/danyim/indecks" target="_blank"><i class="fa fa-github"></i></a></h4>
-            <p>Interactive index card study aid built with ReactJS</p>
-        </figcaption>
-    </div>
-    <div class="portfolio-item">
-        <figure>
-            <h4 class="ital">
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON COMING SOON</span>
-                <!-- <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON COMING SOON
-                COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
-                COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
-                COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
-                COMING SOON COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span>
-                COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
-                COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
-                COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
-                <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON COMING SOON
-                COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
-                COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
-                COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
-                COMING SOON COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> -->
-            </h4>
-        </figure>
-        <figcaption>
-            <h4>
-                ARTGORITHMS
-                <a href="https://github.com/danyim/artgorithms" target="_blank">
-                    <i class="fa fa-github"></i>
-                </a>
-            </h4>
-            <p>A design & art project, to be released soon</p>
-        </figcaption>
-    </div>
-    <div class="portfolio-item">
-        <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/consultantspeak-1024x876.png');">
-        </figure>
-        <figcaption>
-            <h4>
-                Consultant Speak
-                <a href="https://github.com/danyim/ConsultantSpeak" target="_blank">
-                    <i class="fa fa-github"></i>
-                </a>
-            </h4>
-            <p>Random definitions of "consultant speak" built with an Angular, Node/Express, and MongoDB</p>
-        </figcaption>
-    </div>
-    <div class="portfolio-item">
-        <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/gerb-1024x691.png');">
-        </figure>
-        <figcaption>
-            <h4>
-                gerb dawg
-                <a href="https://github.com/danyim/gerbdawg" target="_blank">
-                    <i class="fa fa-github"></i>
-                </a>
-            </h4>
-            <p>Gerb or The Big Dawg--which are you? Made with love using Angular and Firebase</p>
-        </figcaption>
-    </div>
-    <div class="portfolio-item">
-        <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/bao-1024x984.png');">
-        </figure>
-        <figcaption>
-            <h4>
-                Breaking Bao
-                <a href="https://github.com/danyim/breaking-bao" target="_blank">
-                    <i class="fa fa-github"></i>
-                </a>
-            </h4>
-            <p>
-                Public website for a food truck concept, built in Angular and powered by Wordpress for the CMS
-            </p>
-        </figcaption>
+<section class="container portfolio-container">
+    <div class="portfolio">
+        <div class="portfolio-item">
+            <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/indecks-1024x672.png');">
+            </figure>
+            <figcaption>
+                <h4>indecks <a href="https://github.com/danyim/indecks" target="_blank"><i class="fa fa-github"></i></a></h4>
+                <p>Interactive index card study aid built with ReactJS</p>
+            </figcaption>
+        </div>
+        <div class="portfolio-item">
+            <figure>
+                <h4 class="ital">
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON COMING SOON COMING SOON COMING SOON</span>
+                    <!-- <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON COMING SOON
+                    COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
+                    COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
+                    COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
+                    COMING SOON COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span>
+                    COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
+                    COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
+                    COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
+                    <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON COMING SOON
+                    COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON COMING SOON
+                    COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON COMING SOON
+                    COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> COMING SOON
+                    COMING SOON COMING SOON COMING SOON COMING SOON <span class="grey">COMING SOON</span> -->
+                </h4>
+            </figure>
+            <figcaption>
+                <h4>
+                    ARTGORITHMS
+                    <a href="https://github.com/danyim/artgorithms" target="_blank">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </h4>
+                <p>A design & art project, to be released soon</p>
+            </figcaption>
+        </div>
+        <div class="portfolio-item">
+            <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/consultantspeak-1024x876.png');">
+            </figure>
+            <figcaption>
+                <h4>
+                    Consultant Speak
+                    <a href="https://github.com/danyim/ConsultantSpeak" target="_blank">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </h4>
+                <p>Random definitions of "consultant speak" built with an Angular, Node/Express, and MongoDB</p>
+            </figcaption>
+        </div>
+        <div class="portfolio-item">
+            <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/gerb-1024x691.png');">
+            </figure>
+            <figcaption>
+                <h4>
+                    gerb dawg
+                    <a href="https://github.com/danyim/gerbdawg" target="_blank">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </h4>
+                <p>Gerb or The Big Dawg--which are you? Made with love using Angular and Firebase</p>
+            </figcaption>
+        </div>
+        <div class="portfolio-item">
+            <figure style="background-image: url('http://yimd.net/wp-content/uploads/2016/08/bao-1024x984.png');">
+            </figure>
+            <figcaption>
+                <h4>
+                    Breaking Bao
+                    <a href="https://github.com/danyim/breaking-bao" target="_blank">
+                        <i class="fa fa-github"></i>
+                    </a>
+                </h4>
+                <p>
+                    Public website for a food truck concept, built in Angular and powered by Wordpress for the CMS
+                </p>
+            </figcaption>
+        </div>
     </div>
 </section>
 <?php get_sidebar(); ?>
