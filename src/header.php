7ba5974d7924dba1e906226c1c9d95a8da4cb1c3
Daniel Yim <danielyim@gmail.com>
2017-03-11 02:11:53 -0800

    nav is working beautifully

--- src/header.php
+++ src/header.php
@@ -27,7 +27,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">yimd</a>
+              <a class="navbar-brand" href="#">y</a>
             </div> -->
 
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
