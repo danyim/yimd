0faac9d17b5cef5eb6ab43aab1e60078d86be784
Daniel Yim <danielyim@gmail.com>
2020-10-10 01:28:38 -0700

    Load Bootstrap from CDN

--- src/header.php
+++ src/header.php
@@ -12,7 +12,8 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo('pingback_url');?>">
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+ 
 <?php wp_head();?>
 </head>
 
